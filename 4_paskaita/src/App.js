eb41e9d7cdc31251bb897457a481c2bede9f6190
Veronika Tarasova <veronika8ta@gmail.com>
2023-05-23 22:57:39 +0300

    update

--- 4_paskaita/src/App.js
+++ 4_paskaita/src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-import Product from './components/Product';
+import "./App.css";
+import Product from "./components/Product";
 
 function App() {
   return (
-    <div >
-      <Product srcLeft={"https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2019/01/09094322/RS_QUATTRO-crop-500x707.jpg"} title={"Motorsport Artwork Short Graphic Story: Peugeot 205 T16 "} srcRight={"https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2019/01/09093743/RS_205-crop-500x703.jpg"} price={10} description={<p>Unique designs <br /><br /> All pieces are printed on heavyweight 200 gsm art paper <br /><br /> Limited to 50 pieces</p>} availability={"10"} category={"Posters"} />
+    <div>
+      <Product
+        srcLeft={
+          "https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2019/01/09094322/RS_QUATTRO-crop-500x707.jpg"
+        }
+        title={"Motorsport Artwork Short Graphic Story: Peugeot 205 T16 "}
+        srcRight={
+          "https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2019/01/09093743/RS_205-crop-500x703.jpg"
+        }
+        price={10}
+        description={
+          <p>
+            Unique designs <br />
+            <br /> All pieces are printed on heavyweight 200 gsm art paper{" "}
+            <br />
+            <br /> Limited to 50 pieces
+          </p>
+        }
+        availability={10}
+        category={"Posters"}
+      />
     </div>
   );
 }
--- 4_paskaita/src/components/Product.jsx
+++ 4_paskaita/src/components/Product.jsx
@@ -1,63 +1,70 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
-import {FaArrowLeft, FaArrowRight}  from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-const Product = ({srcLeft, srcRight, title,price,description,availability, category}) => {
+const Product = ({
+  srcLeft,
+  srcRight,
+  title,
+  price,
+  description,
+  availability,
+  category,
+}) => {
   const [quantity, setQuantity] = useState(0);
 
-  const increaseNumber = () =>{
-    setQuantity(quantity+1)
-}
+  const increaseNumber = () => {
+    setQuantity(quantity + 1);
+  };
 
-const decreaseNumber = () =>{
-    setQuantity(quantity-1)
-}
-
+  const decreaseNumber = () => {
+    setQuantity(quantity - 1);
+  };
 
   return (
     <div className="card flex">
-        <div className="pictures">
+      <div className="pictures">
         <img src={srcLeft} alt={title} />
         <img src={srcRight} alt={title} />
+      </div>
+      <div className="information">
+        <div className="flex">
+          <h3>{title}</h3>
+          <div className="flex">
+            <FaArrowLeft className="arrow" />
+            <FaArrowRight className="arrow" />
+          </div>
         </div>
-        <div className="information">
-            <div className="flex">
-            <h3>{title}</h3>
-            <div className="flex">
-            <FaArrowLeft className="arrow"/>
-            <FaArrowRight className="arrow"/> 
-            </div>
-           
-            </div>
-            <h4>{price}.00$</h4>
-            <p>{description}</p>
-            <h4>Availability:<span> {availability} in stock</span></h4>
-            <div className="flex buttons">
-                <div className="flex smallButtons">
-                    <button onClick = {decreaseNumber}>-</button>
-                    <h5>{quantity}</h5>
-                    <button onClick = {increaseNumber}>+</button>
-                </div>
-                <div>
-                    <Button />
-                </div>
-            </div>
-             <h4 className="category">Category: {category}</h4>
+        <h4>{price}.00$</h4>
+        <p>{description}</p>
+        <h4>
+          Availability:<span> {availability} in stock</span>
+        </h4>
+        <div className="flex buttons">
+          <div className="flex smallButtons">
+            <button onClick={decreaseNumber}>-</button>
+            <h5>{quantity}</h5>
+            <button onClick={increaseNumber}>+</button>
+          </div>
+          <div>
+            <Button />
+          </div>
         </div>
-      
+        <h4 className="category">Category: {category}</h4>
+      </div>
     </div>
   );
 };
 
 Product.propTypes = {
-    srcRight: PropTypes.string,
-    srcLeft: PropTypes.string,
-   title:PropTypes.string,
-   price:PropTypes.number,
-   description:PropTypes.string,
-   availability:PropTypes.number,
-   category:PropTypes.string,
-  };
-  
+  srcRight: PropTypes.string,
+  srcLeft: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
+//   description: PropTypes.string,
+  availability: PropTypes.number,
+  category: PropTypes.string,
+};
+
 export default Product;
